c226d5340d942c411ba89ceba8c0d48d4f4351bf
karen sanchez <karen.s.sanchez08@gmail.com>
2016-07-11 18:11:46 -0400

    looking good

--- script.js
+++ script.js
@@ -1,53 +1,17 @@
 $(document).ready(function() {
-
-
-    $('.hola').hide().show(1000);
-    	$('.top').hide().show(2300);
-            $('.middle').hide().show(3300);
-                $('.bottom').hide().show(4000);
-
-    setTimeout(function() {
-        $('.hola').css('color', '#ff5050');
-    }, 3000);
-
-    setTimeout(function() {
-        $('.top').css('color', '#ff5050');
-    }, 3500);
-
-    setTimeout(function() {
-        $('.middle').css('color', '#ff5050');
-
-    }, 4000);
-
-    setTimeout(function() {
-        $('.bottom').css('color', '#ff5050');
-    }, 4500);
-
-    $('#bio h3').css('display', 'block');
-
 }) // End document.ready
 
-    $(window).scroll(function(){
+$(window).scroll(function(){
 
-        var wScroll = $(this).scrollTop();
-
-				// console.log(wScroll);
-	// if($(window).scrollTop() > 1365){
-            //  console.log('hello');
-        //     $('#skills').
-        // };
-
-            if($(this).scrollTop() > 800){
-            $('.scrollup').fadeIn(3000);
-        } else {
-            $('.scrollup').fadeOut();
+    var wScroll = $(this).scrollTop();
+        if (wScroll == 0) {
+            $(function(){
+                $(".element").typed({
+                  strings: ["Welcome,", "my name is...", "Karen", "Sanchez", "Karen Sanchez"],
+                  typeSpeed: 0
+                });
+            });
         }
-    });
-    $('.scrollup').click(function(){
-        $('html, body').animate({
-            scrollTop:0
-        }, 800);
-        return false;
     });
 
     $(function(){
