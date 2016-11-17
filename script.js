d8b8a0a3248ba6819fab24120b1e07ba073b654c
karen sanchez <karen.s.sanchez08@gmail.com>
2016-11-17 12:58:37 -0500

    last push

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 $(document).ready(function(){
 
+	$(".hello-box").hide().fadeIn(3000);
 	
-});
+	function isElementVisible($elementToBeChecked)
+	{
+	    var TopView = $(window).scrollTop();
+	    var BotView = TopView + $(window).height();
+	    var TopElement = $elementToBeChecked.offset().top;
+	    var BotElement = TopElement + $elementToBeChecked.height();
+	    return ((BotElement <= BotView) && (TopElement >= TopView));
+	}
 
+	$(window).scroll(function () {
+	    $( ".slide" ).each(function() {
+	        $this = $(this);
+	        isOnView = isElementVisible($(this));
+	        if(isOnView && !$(this).hasClass('Starting')){
+	            $(this).addClass('Starting');
+	            startAnimation($(this));
+	        }
+	    });
+	});
+
+	function startAnimation($this) {
+	  $this.animate({
+	    right: '720px'
+	  }, 1000, function() {
+	    // Animation complete.
+	  });
+	}
+
+});
+	
