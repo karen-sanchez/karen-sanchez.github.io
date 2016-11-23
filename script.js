017f6c5a0cc86a8a1ebe7787db1b6892793807d6
karen sanchez <karen.s.sanchez08@gmail.com>
2016-11-23 15:20:24 -0500

    inview

--- script.js
+++ script.js
@@ -11,16 +11,33 @@ $(document).ready(function(){
 	    return ((BotElement <= BotView) && (TopElement >= TopView));
 	}
 
-	$(window).scroll(function () {
-	    $( ".slide").each(function() {
-	        // $this = $(this);
-	        isOnView = isElementVisible($(this));
-	        if(isOnView){
-	            $(".slide").fadeIn(1500);
-	            // startAnimation();
-	        }
-	    });
+	$('#about').on('inview', function(event, isInView) {
+	  if (isInView) {
+	  	$('#about').hide().fadeIn(2000);
+	    console.log("inview works")
+	  } else {
+	    $('#about').off('inview');
+	  }
 	});
+	$('#skills').on('inview', function(event, isInView) {
+	  if (isInView) {
+	  	$('#skills').hide().show(2000);
+	    console.log("inview works")
+	  } else {
+	    $('#skills').off('inview');
+	  }
+	});
+
+	// $(window).scroll(function () {
+	//     $( ".slide").each(function() {
+	//         // $this = $(this);
+	//         isOnView = isElementVisible($(this));
+	//         if(isOnView){
+	//             $(".slide").fadeIn(1500);
+	//             // startAnimation();
+	//         }
+	//     });
+	// });
 
 	// function startAnimation() {
 	// 	$(".slide").fadeIn(1500);
