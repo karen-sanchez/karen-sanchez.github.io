834b60a8497a798c107a957637242e17fb7fe310
Karen Sanchez <karensanchez@ChristohersMBP3.home>
2017-02-25 12:17:57 -0500

    commit

--- script.js
+++ script.js
@@ -10,27 +10,27 @@ $(document).ready(function(){
 	    var BotElement = TopElement + $elementToBeChecked.height();
 	    return ((BotElement <= BotView) && (TopElement >= TopView));
 	}	
-	$('#circles').on('inview', function(event, isInView) {
-	  if (isInView) {
-	  	$('#circles').addClass('animated fadeInUp');
-	   } else {
-	  	$('#circles').off('inview');
-	  }
-	});
-	$('#skills').on('inview', function(event, isInView) {
-	  if (isInView) {
-	  	$('#skills').addClass('animated fadeInUp');
-	   } else {
-	  	$('#skills').off('inview');
-	  }
-	});
-	$('#projects').on('inview', function(event, isInView) {
-	  if (isInView) {
-	  	$('#projects').addClass('animated fadeInUp');
-	  } else {
-	    $('#projects').off('inview');
-	  }
-	});
+	// $('#circles').on('inview', function(event, isInView) {
+	//   if (isInView) {
+	//   	$('#circles').addClass('animated fadeInUp');
+	//    } else {
+	//   	$('#circles').off('inview');
+	//   }
+	// });
+	// $('#skills').on('inview', function(event, isInView) {
+	//   if (isInView) {
+	//   	$('#skills').addClass('animated fadeInUp');
+	//    } else {
+	//   	$('#skills').off('inview');
+	//   }
+	// });
+	// $('#projects').on('inview', function(event, isInView) {
+	//   if (isInView) {
+	//   	$('#projects').addClass('animated fadeInUp');
+	//   } else {
+	//     $('#projects').off('inview');
+	//   }
+	// });
 	$('.spinner').delay(6000).fadeOut(400);
 	$('.main-logo').delay(7000).fadeIn(500);
 });
