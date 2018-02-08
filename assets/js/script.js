f3e07a3a2683a5a4cdcae137244d6f1f2bd35c39
karen sanchez <karen.s.sanchez08@gmail.com>
2018-02-07 20:20:27 -0500

    Added new favicon. Continued to work on responsiveness. More projects btn now displays remaining two projects.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 800) {
-                $('.back-to-top').fadeIn();
-            } else {
-                $('.back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('.back-to-top').click(function () {
-        	console.log('clicked')
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 800) {
+			$('.back-to-top').fadeIn();
+		} else {
+			$('.back-to-top').fadeOut();
+		}
+	});
+	// scroll body to 0px on click
+	$('.back-to-top').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 1800);
+		return false;
+	});
+
+	$('.hidden-project').hide();
+	$('.btn-more').click(function(){
+		$('.hidden-project').show('slow');
+		console.log('more clicked');
+	});
 });
