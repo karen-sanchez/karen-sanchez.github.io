2f6a0139f0b70b6be6e159595e58dbf28fc10a80
Karen Sanchez <karensanchez@Karens-MacBook-Pro.local>
2019-08-04 12:58:19 -0400

    Fixed:getBoundingClientRect() returning empty on smaller screen widths

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 //if user scrolled to bottom do something
 window.onscroll = function() {
     if( (window.innerHeight + window.scrollY) >= document.body.offsetHeight - 100){
-        // console.log('bottom');
-        //get back to top element
-        document.querySelector('.back-to-top img').style.transform = "rotate(-90deg)";
+        //get next-section element
+        document.querySelector('.next-section img').style.transform = "rotate(-90deg)";
     } else {
-        document.querySelector('.back-to-top img').style.transform = "rotate(90deg)";
+        document.querySelector('.next-section img').style.transform = "rotate(90deg)";
     }
 };
 
-//Resusable
-var isInViewport = function(elem) {
-    var bounding = elem.getBoundingClientRect();
-    return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
-
 //Arrow scrolls user to next section
-document.querySelector('.back-to-top').addEventListener('click', function() {
+document.querySelector('.next-section').addEventListener('click', function() {
 
     var whoSection = document.querySelector('.who-title');
     var whatSection = document.querySelector('.what-title');
@@ -42,3 +30,13 @@ document.querySelector('.back-to-top').addEventListener('click', function() {
     }
 });
 
+//Resusable
+function isInViewport(elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= -4 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
